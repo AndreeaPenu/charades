aae7c3438804fd375492ccee4b3929cd976d3b53
manarkbyeh <manar.kbyeh@student.kdg.be>
2017-12-09 00:17:52 +0100

    model

--- charades/backend/app/participator.php
+++ charades/backend/app/participator.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App;
-
+use App\correct_answer;
 use Illuminate\Database\Eloquent\Model;
 
 class participator extends Model
 {
-    //
+    public function answers() {
+        return $this->hasMany(correct_answer::class);
+    }
 }
