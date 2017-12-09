22a8d7459ed421d86f203c63af0ad856c2b49edc
manarkbyeh <manar.kbyeh@student.kdg.be>
2017-12-09 01:03:00 +0100

    name

--- charades/backend/app/participator.php
+++ charades/backend/app/participator.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class participator extends Model
 {
+    public $fillable = [
+        'name',
+     
+    ];
     public function answers() {
         return $this->hasMany(correct_answer::class);
     }
