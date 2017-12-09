7ae78a928459ceaba742ec8fc9ae9d07a7d2822a
andreea penu <andreea_penu@hotmail.com>
2017-12-09 01:43:13 +0100

    word check

--- charades/backend/app/Http/Controllers/wordController.php
+++ charades/backend/app/Http/Controllers/wordController.php
@@ -9,13 +9,23 @@ class wordController extends Controller
 {
 
     public function index(){
-        $words = word::all(); //Not a good idea
-        return $words;
+        //$words = word::all(); //Not a good idea
+        //return $words;
+        $randWord = $this->getRandomWord();
+        return $randWord;
     }
 
     public function getRandomWord(){
         $random_word = word::all()->random(1);
         return $random_word;
+    }
+
+    public function checkIfWordIsCorrect($given_word,$correct_word){
+        if($given_word == $correct_word){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 
