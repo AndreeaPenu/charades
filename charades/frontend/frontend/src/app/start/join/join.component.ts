46a482f7ac64a6d9a4287feb7fa3eb64405172ac
jonasvanloey <jonas.vanloey@student.kdg.be>
2017-12-09 07:44:52 +0100

    added sessioncontroller

--- charades/frontend/frontend/src/app/start/join/join.component.ts
+++ charades/frontend/frontend/src/app/start/join/join.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from "@angular/forms";
-import {Router} from "@angular/router";
+
 import {Http} from "@angular/http";
 import {Router} from "@angular/router";
 
